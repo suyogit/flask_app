7d39fb3b566ddc007b91d33753e31e438663b403
suyoyog <asuyog042@gmail.com>
2024-04-28 14:46:16 +0545

    login and registration page done

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    return "<h1 style='color:green'>Hellow suyog</h1>"
+    return render_template('login.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 app.run(debug=True)#so that we dont have o refresh multiple time
