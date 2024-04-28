e0c6236e86dcb8309dc6cadee6d7fd5f4564bc74
suyoyog <asuyog042@gmail.com>
2024-04-28 15:50:08 +0545

    user to flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,5 +8,16 @@ def index():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+
+@app.route('/perform_registration',methods=['post'])
+def perform_registration():
+    name= request.form.get('name')
+    email=request.form.get('email')
+    password= request.form.get('password')
+
+    return name + " "+ email+ " " + password
+
+
 
 app.run(debug=True)#so that we dont have o refresh multiple time
